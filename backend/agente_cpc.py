8ab41cf1fe81d59d0425bf5152f9037642f6d0e9
Jonata Henrique <jonatah.1512@gmail.com>
2025-11-16 12:25:23 -0300

    att prompt

--- backend/agente_cpc.py
+++ backend/agente_cpc.py
@@ -334,7 +334,7 @@ def traduzir_para_nl(data: dict):
             Você é um assistente que cria glossários.
             Gere um glossário simples em português para as variáveis lógicas: {atomos}.
             As definições devem ser frases afirmativas curtas, claras e naturais.
-            Dê preferência a definições SIMPLES, cotidianas, e não ambíguas mas *QUE SE ENCAIXEM EM UM MESMO CONTEXTO*.
+            Dê preferência a definições SIMPLES, cotidianas, e não ambíguas.
             Responda sem explicações ou texto adicional.
             
             Responda APENAS com o objeto JSON.
